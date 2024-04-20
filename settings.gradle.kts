4a891b13da31444b94f19d0e6af0ae92e9db1804
raransyw <jeonnkook12@gmail.com>
2024-04-20 18:37:10 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UTSANMP_160421068"
+include(":app")
+ 
